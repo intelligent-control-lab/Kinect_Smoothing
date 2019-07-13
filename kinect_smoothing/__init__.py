2141f915f369644a64b63a285f2e7b9e4dcb21b7
Abuduweili <37198574+Walleclipse@users.noreply.github.com>
2019-07-13 10:03:44 -0400

    Update __init__.py

--- kinect_smoothing/__init__.py
+++ kinect_smoothing/__init__.py
@@ -1,5 +1,5 @@
 
 from .depth_image_smoothing import HoleFilling_Filter, Denoising_Filter
-from .trajectory_smoothing import Crop_Filter, Smooth_Filter, Motion_Sampler
+from .trajectory_smoothing import Crop_Filter, Smooth_Filter, Motion_Sampler, GradientCrop_Filter
 from .coordinate_calculation import Coordinate_Calculator
 from .pipeline import Kinect_Openpose_Pipeline
